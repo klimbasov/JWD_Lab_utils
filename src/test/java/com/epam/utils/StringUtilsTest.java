fa31d36aacfb2abaa862cf4a01554c1fdb92c3a3
klimbasov <klimbasov.al@gmail.com>
2022-04-01 11:19:43 +0300

    small fixes

--- src/test/java/com/epam/utils/StringUtilsTest.java
+++ src/test/java/com/epam/utils/StringUtilsTest.java
@@ -8,15 +8,9 @@ class StringUtilsTest {
 
     @Test
     void isPositiveNumber() {
-        String[] exceptionInputs = {"", null, "a", "sd", "--3", "a4", "4a", "4a4", "-3-", "34 5", " 456", "234 ", ".234",
-                "234.", "123..123", "12. 321", "123 .123", "2-3", "-0"};
         String[] trueInputs = {"123", "0.545", "123.123", "2"};
-        String[] falseInputs = {"-123", "-0.34", "-123.132", "-3", "0"};
-
-        for (String input :
-                exceptionInputs) {
-            assertThrows(NumberFormatException.class, () -> StringUtils.isPositiveNumber(input));
-        }
+        String[] falseInputs = {"-123", "-0.34", "-123.132", "-3", "0", "", null, "a", "sd", "--3", "a4", "4a", "4a4", "-3-", "34 5", " 456", "234 ", ".234",
+                "234.", "123..123", "12. 321", "123 .123", "2-3", "-0"};
 
         for (String input :
                 trueInputs) {
